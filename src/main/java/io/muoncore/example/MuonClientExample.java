932b1cda933976ea620706fe66405899648c6f70
David Dawson <david.dawson@simplicityitself.com>
2016-03-10 12:13:51 +0000

    use muon bulder.

--- src/main/java/io/muoncore/example/MuonClientExample.java
+++ src/main/java/io/muoncore/example/MuonClientExample.java
@@ -1,24 +1,10 @@
 package io.muoncore.example;
 
-import io.muoncore.Discovery;
 import io.muoncore.Muon;
-import io.muoncore.SingleTransportMuon;
-import io.muoncore.codec.Codecs;
-import io.muoncore.codec.json.JsonOnlyCodecs;
+import io.muoncore.MuonBuilder;
 import io.muoncore.config.AutoConfiguration;
-import io.muoncore.extension.amqp.*;
-import io.muoncore.extension.amqp.discovery.AmqpDiscovery;
-import io.muoncore.extension.amqp.rabbitmq09.RabbitMq09ClientAmqpConnection;
-import io.muoncore.extension.amqp.rabbitmq09.RabbitMq09QueueListenerFactory;
+import io.muoncore.config.MuonConfigBuilder;
 import io.muoncore.protocol.requestresponse.Response;
-import io.muoncore.transport.MuonTransport;
-import io.muoncore.transport.ServiceCache;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Properties;
 
 
 /**
@@ -28,56 +14,15 @@ public class MuonClientExample {
 
     public static void main(String[] args) throws Exception {
 
-        Properties props = new Properties();
-        props.load(MuonClientExample.class.getResourceAsStream("/application.properties"));
+        AutoConfiguration config = MuonConfigBuilder.withServiceIdentifier("example-service-client").build();
 
-        Muon muon = createMuon(props);
+        Muon muon = MuonBuilder.withConfig(config).build();
         muon.getDiscovery().blockUntilReady();
 
-        //a bug in the discovery means the above block doesn't work. Fixed in next snapshot
-        Thread.sleep(4000);
-
-        Response<String> data = muon.request("request://ExampleService/hello", String.class).get();
+        Response<String> data = muon.request("request://example-service/hello", String.class).get();
 
         System.out.println("Example Service says: " + data.getPayload());
 
         muon.shutdown();
-    }
-
-    //the below will be folded into a property based builder in a future version
-    private static Muon createMuon(Properties props) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, IOException {
-
-        String serviceName = props.getProperty("service.name") + "-client";
-        String amqpUrl = props.getProperty("amqp.url");
-
-        Discovery discovery = createDiscovery(amqpUrl);
-        MuonTransport transport = createTransport(serviceName, amqpUrl);
-
-        AutoConfiguration config = new AutoConfiguration();
-        config.setServiceName(serviceName);
-        config.setAesEncryptionKey("abcde12345678906");
-        
-        return new SingleTransportMuon(config, discovery, transport);
-    }
-
-    private static MuonTransport createTransport(String serviceName, String amqpUrl) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, IOException {
-        AmqpConnection connection = new RabbitMq09ClientAmqpConnection(amqpUrl);
-        QueueListenerFactory queueFactory = new RabbitMq09QueueListenerFactory(connection.getChannel());
-
-        ServiceQueue serviceQueue = new DefaultServiceQueue(serviceName, connection);
-        AmqpChannelFactory channelFactory = new DefaultAmqpChannelFactory(serviceName, queueFactory, connection);
-
-        return new AMQPMuonTransport(amqpUrl, serviceQueue, channelFactory);
-    }
-
-    private static Discovery createDiscovery(String amqpUrl) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, IOException {
-        AmqpConnection connection = new RabbitMq09ClientAmqpConnection(amqpUrl);
-        QueueListenerFactory queueFactory = new RabbitMq09QueueListenerFactory(connection.getChannel());
-
-        Codecs codecs = new JsonOnlyCodecs();
-
-        AmqpDiscovery discovery = new AmqpDiscovery(queueFactory, connection, new ServiceCache(), codecs);
-        discovery.start();
-        return discovery;
     }
 }
--- src/main/java/io/muoncore/example/MuonServerExample.java
+++ src/main/java/io/muoncore/example/MuonServerExample.java
@@ -1,25 +1,13 @@
 package io.muoncore.example;
 
-import io.muoncore.Discovery;
 import io.muoncore.Muon;
-import io.muoncore.SingleTransportMuon;
-import io.muoncore.codec.Codecs;
-import io.muoncore.codec.json.JsonOnlyCodecs;
+import io.muoncore.MuonBuilder;
 import io.muoncore.config.AutoConfiguration;
-import io.muoncore.extension.amqp.*;
-import io.muoncore.extension.amqp.discovery.AmqpDiscovery;
-import io.muoncore.extension.amqp.rabbitmq09.RabbitMq09ClientAmqpConnection;
-import io.muoncore.extension.amqp.rabbitmq09.RabbitMq09QueueListenerFactory;
-import io.muoncore.transport.MuonTransport;
-import io.muoncore.transport.ServiceCache;
+import io.muoncore.config.MuonConfigBuilder;
 import reactor.rx.broadcast.Broadcaster;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import static io.muoncore.protocol.requestresponse.server.HandlerPredicates.all;
 
@@ -32,23 +20,23 @@ public class MuonServerExample {
 
     public static void main(String[] args) throws Exception {
 
-        Properties props = new Properties();
-        props.load(MuonServerExample.class.getResourceAsStream("/application.properties"));
-
-        muonServer(props);
+        muonServer();
 
         Thread.sleep(2000);
 
         System.out.println("Use Ctrl-C to exit");
     }
 
-    private static Muon muonServer(Properties props) throws Exception {
+    private static Muon muonServer() throws Exception {
+        AutoConfiguration config = MuonConfigBuilder.withServiceIdentifier("example-service").build();
 
-        Muon muon = createMuon(props);
+        Muon muon = MuonBuilder.withConfig(config).build();
 
         muon.handleRequest(all(), Map.class, request -> {
             System.out.println("A request has been made ");
-            request.ok("The answer is yes");
+            Map data = new HashMap();
+            data.put("Hello", "world " + System.currentTimeMillis());
+            request.ok(data);
         });
 
 //        Broadcaster<String> stream = Broadcaster.create();
@@ -75,41 +63,5 @@ public class MuonServerExample {
             }
 
         }).start();
-    }
-
-    private static Muon createMuon(Properties props) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, IOException {
-
-        String serviceName = props.getProperty("service.name");
-        String amqpUrl = props.getProperty("amqp.url");
-
-        Discovery discovery = createDiscovery(amqpUrl);
-        MuonTransport transport = createTransport(serviceName, amqpUrl);
-
-        AutoConfiguration config = new AutoConfiguration();
-        config.setServiceName(serviceName);
-        config.setAesEncryptionKey("abcde12345678906");
-
-        return new SingleTransportMuon(config, discovery, transport);
-    }
-
-    private static MuonTransport createTransport(String serviceName, String amqpUrl) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, IOException {
-        AmqpConnection connection = new RabbitMq09ClientAmqpConnection(amqpUrl);
-        QueueListenerFactory queueFactory = new RabbitMq09QueueListenerFactory(connection.getChannel());
-
-        ServiceQueue serviceQueue = new DefaultServiceQueue(serviceName, connection);
-        AmqpChannelFactory channelFactory = new DefaultAmqpChannelFactory(serviceName, queueFactory, connection);
-
-        return new AMQPMuonTransport(amqpUrl, serviceQueue, channelFactory);
-    }
-
-    private static Discovery createDiscovery(String amqpUrl) throws URISyntaxException, KeyManagementException, NoSuchAlgorithmException, IOException {
-        AmqpConnection connection = new RabbitMq09ClientAmqpConnection(amqpUrl);
-        QueueListenerFactory queueFactory = new RabbitMq09QueueListenerFactory(connection.getChannel());
-
-        Codecs codecs = new JsonOnlyCodecs();
-
-        AmqpDiscovery discovery = new AmqpDiscovery(queueFactory, connection, new ServiceCache(), codecs);
-        discovery.start();
-        return discovery;
     }
 }
